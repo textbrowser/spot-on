f06ccf01ea490fc1d5687775afa693971471ef01
Alexis Megas <textbrowser@gmail.com>
2015-04-30 17:08:57 -0400

    Next version.

--- branches/trunk/Common/spot-on-common.h
+++ branches/trunk/Common/spot-on-common.h
@@ -30,7 +30,7 @@
 #ifndef _spoton_common_h_
 #define _spoton_common_h_
 
-#define SPOTON_VERSION_STR "0.22"
+#define SPOTON_VERSION_STR "0.23"
 
 typedef QHash<QString, QByteArray> QStringByteArrayHash;
 typedef QList<QByteArray> QByteArrayList;
