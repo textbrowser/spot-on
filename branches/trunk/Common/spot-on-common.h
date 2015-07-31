58db2609e8ffa51af33397778e10828990dd239e
Alexis Megas <textbrowser@gmail.com>
2015-07-31 17:47:49 -0400

    Updated release date.

--- branches/trunk/Common/spot-on-common.h
+++ branches/trunk/Common/spot-on-common.h
@@ -31,7 +31,7 @@
 #ifndef _spoton_common_h_
 #define _spoton_common_h_
 
-#define SPOTON_VERSION_STR "2015.07.31"
+#define SPOTON_VERSION_STR "2015.08.01"
 
 typedef QHash<QString, QByteArray> QStringByteArrayHash;
 typedef QList<QByteArray> QByteArrayList;
