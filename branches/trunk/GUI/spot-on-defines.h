756285d9b8eb21ae485644e25bb53646545cd807
Alexis Megas <textbrowser@gmail.com>
2015-08-07 08:04:11 -0400

    Release information.

--- branches/trunk/GUI/spot-on-defines.h
+++ branches/trunk/GUI/spot-on-defines.h
@@ -33,7 +33,7 @@
 #undefine SPOTON_APPLICATION_NAME
 #endif
 #define SPOTON_APPLICATION_NAME "GoldBug"
-#define SPOTON_GOLDBUG_VERSION_STR "GB v. 3.0"
+#define SPOTON_GOLDBUG_VERSION_STR "GB v. 2.7"
 #else
 #define SPOTON_APPLICATION_NAME "Spot-On"
 #endif
