ac89df56427d510927a056d0f3fde9297651ab09
Guess Who <textbrowser@gmail.com>
2015-08-15 13:10:57 -0400

    Corrected.

--- branches/trunk/spot-on.sh
+++ branches/trunk/spot-on.sh
@@ -3,7 +3,7 @@
 if [ -r /usr/local/spot-on/Spot-On ] && [ -x /usr/local/spot-on/Spot-On ]
 then
     export LD_LIBRARY_PATH=/usr/local/spot-on/Lib
-    cd /usr/local/spot-on/Spot-On && exec ./Spot-On -style fusion "$@"
+    cd /usr/local/spot-on && exec ./Spot-On -style fusion "$@"
     exit $?
 else
     exit 1
