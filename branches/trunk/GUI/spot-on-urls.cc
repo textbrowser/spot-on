95865235372de46c4ca8678cb01a82b20bf41044
Guess Who <textbrowser@gmail.com>
2015-10-11 12:57:42 -0400

    Updated.

--- branches/trunk/GUI/spot-on-urls.cc
+++ branches/trunk/GUI/spot-on-urls.cc
@@ -1636,7 +1636,7 @@ void spoton::slotUrlLinkClicked(const QUrl &u)
 	  QToolTip::showText(pos(), "");
 	  QToolTip::showText
 	    (pos(),
-	     tr("<html><h4>Link %1 shared with your friendly "
+	     tr("<html><h4>URL %1 shared with your friendly "
 		"participants.</h4></html>").
 	     arg(original.toEncoded().constData()));
 	}
