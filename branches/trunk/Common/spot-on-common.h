f7bdb408ec38e87269ce85fb44efaa524b08512e
Guess Who <textbrowser@gmail.com>
2015-11-24 13:15:29 -0500

    New release date.

--- branches/trunk/Common/spot-on-common.h
+++ branches/trunk/Common/spot-on-common.h
@@ -31,7 +31,7 @@
 #include <QHash>
 #include <QStringList>
 
-#define SPOTON_VERSION_STR "2015.11.24"
+#define SPOTON_VERSION_STR "2015.11.25"
 
 typedef QHash<QString, QByteArray> QStringByteArrayHash;
 typedef QList<QByteArray> QByteArrayList;
