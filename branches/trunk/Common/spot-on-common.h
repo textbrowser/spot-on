c47fe4fdd676c7917f7d11d158b04d52fdac7355
Alexis Megas <textbrowser@gmail.com>
2015-11-26 09:40:57 -0500

    Common date.

--- branches/trunk/Common/spot-on-common.h
+++ branches/trunk/Common/spot-on-common.h
@@ -31,7 +31,7 @@
 #include <QHash>
 #include <QStringList>
 
-#define SPOTON_VERSION_STR "2015.11.27"
+#define SPOTON_VERSION_STR "2015.11.28"
 
 typedef QHash<QString, QByteArray> QStringByteArrayHash;
 typedef QList<QByteArray> QByteArrayList;
