773e36cae62a7a7846ebb1533bb04cfc8fce5219
Monster <textbrowser@gmail.com>
2016-01-18 07:31:37 -0500

    New directory.

---
+++ branches/trunk/Shell/spot-on.qt5.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ -r /usr/local/spot-on/Spot-On ] && [ -x /usr/local/spot-on/Spot-On ]
+then
+    export LD_LIBRARY_PATH=/usr/local/spot-on/Lib
+    cd /usr/local/spot-on && exec ./Spot-On -style fusion "$@"
+    exit $?
+else
+    exit 1
+fi
