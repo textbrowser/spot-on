6f4d979e4e115f135646915d17353136dd05b8ce
Guess Who <textbrowser@gmail.com>
2016-04-30 18:56:31 -0400

    Removed extra qSort().

--- branches/trunk/GUI/spot-on-g.cc
+++ branches/trunk/GUI/spot-on-g.cc
@@ -100,7 +100,6 @@ void spoton::slotRemoveAttachment(const QUrl &url)
   QStringList list(m_ui.attachment->toPlainText().split('\n'));
 
   m_ui.attachment->clear();
-  qSort(list);
 
   while(!list.isEmpty())
     {
