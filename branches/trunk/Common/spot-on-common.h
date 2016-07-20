8ff74ee954dfbe8adcace0c1f11b693460691354
Guess Who <textbrowser@gmail.com>
2016-07-20 10:34:26 -0400

    Release version.

--- branches/trunk/Common/spot-on-common.h
+++ branches/trunk/Common/spot-on-common.h
@@ -31,7 +31,7 @@
 #include <QHash>
 #include <QStringList>
 
-#define SPOTON_VERSION_STR "2016.07.16"
+#define SPOTON_VERSION_STR "2016.08.01"
 
 typedef QHash<QString, QByteArray> QStringByteArrayHash;
 typedef QList<QByteArray> QByteArrayList;
