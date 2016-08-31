6a5070285e5318eaa7d4abaf908a56ae6113bd8c
Guess Who <textbrowser@gmail.com>
2016-08-31 17:47:16 -0400

    New version.

--- branches/trunk/GUI/spot-on-defines.h
+++ branches/trunk/GUI/spot-on-defines.h
@@ -33,7 +33,7 @@
 #undefine SPOTON_APPLICATION_NAME
 #endif
 #define SPOTON_APPLICATION_NAME "GoldBug"
-#define SPOTON_GOLDBUG_VERSION_STR "GB v. 3.0"
+#define SPOTON_GOLDBUG_VERSION_STR "GB v. 3.1"
 #else
 #define SPOTON_APPLICATION_NAME "Spot-On"
 #endif
