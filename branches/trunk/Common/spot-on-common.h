efe5b41a2e85683f90e9e70d0beac99a7aca9005
Monster <textbrowser@gmail.com>
2016-10-08 13:03:09 -0400

    Date may change.

--- branches/trunk/Common/spot-on-common.h
+++ branches/trunk/Common/spot-on-common.h
@@ -31,7 +31,7 @@
 #include <QHash>
 #include <QStringList>
 
-#define SPOTON_VERSION_STR "2016.10.08"
+#define SPOTON_VERSION_STR "2016.11.05"
 
 typedef QHash<QString, QByteArray> QStringByteArrayHash;
 typedef QList<QByteArray> QByteArrayList;
