082a193e6ea480e2dd754bd8856b75648ff2739b
Monster <textbrowser@gmail.com>
2016-11-25 18:07:52 -0500

    New files.

---
+++ libNTL/windows.d/include/NTL/version.h
@@ -0,0 +1,12 @@
+
+#ifndef NTL_version__H
+#define NTL_version__H
+
+#define NTL_VERSION "10.1.0"
+
+#define NTL_MAJOR_VERSION  (10)
+#define NTL_MINOR_VERSION  (1)
+#define NTL_REVISION       (0)
+
+#endif
+
