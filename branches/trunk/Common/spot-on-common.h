6dc8166806cd9c0abb2a6fd3726843e080f2eaf4
Monster <textbrowser@gmail.com>
2017-04-05 15:08:31 -0400

    Perhaps tomorrow.

--- branches/trunk/Common/spot-on-common.h
+++ branches/trunk/Common/spot-on-common.h
@@ -31,7 +31,7 @@
 #include <QHash>
 #include <QStringList>
 
-#define SPOTON_VERSION_STR "2017.03.18"
+#define SPOTON_VERSION_STR "2017.04.06"
 
 typedef QHash<QString, QByteArray> QStringByteArrayHash;
 typedef QList<QByteArray> QByteArrayList;
