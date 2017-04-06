b36b0f0812fbfad3bf5dfa90c2f4d3f9edb9871f
Monster <textbrowser@gmail.com>
2017-04-06 14:12:41 -0400

    New files.

---
+++ branches/trunk/Doxygen/html/search/classes_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['cocoainitializer',['CocoaInitializer',['../classCocoaInitializer.html',1,'']]],
+  ['curl_5fupload_5fstatus',['curl_upload_status',['../structcurl__upload__status.html',1,'']]]
+];
--- branches/trunk/Doxygen/html/search/searchdata.js
+++ branches/trunk/Doxygen/html/search/searchdata.js
@@ -1,12 +1,18 @@
 var indexSectionsWithContent =
 {
+  0: "cps",
+  1: "cps"
 };
 
 var indexSectionNames =
 {
+  0: "all",
+  1: "classes"
 };
 
 var indexSectionLabels =
 {
+  0: "All",
+  1: "Classes"
 };
 
