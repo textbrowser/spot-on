14f4293c2a0f269df089f4eec9d2f6bfd8cd6199
Monster <textbrowser@gmail.com>
2017-05-09 09:26:17 -0400

    New release version.

--- branches/trunk/Common/spot-on-common.h
+++ branches/trunk/Common/spot-on-common.h
@@ -31,7 +31,7 @@
 #include <QHash>
 #include <QStringList>
 
-#define SPOTON_VERSION_STR "2017.04.30"
+#define SPOTON_VERSION_STR "2017.05.10"
 
 typedef QHash<QString, QByteArray> QStringByteArrayHash;
 typedef QList<QByteArray> QByteArrayList;
