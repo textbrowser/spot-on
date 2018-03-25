62a813851e7918c039e0e9c56f56033c42fcba4a
Monster <textbrowser@gmail.com>
2018-03-24 23:19:30 -0400

    Removed methods.

--- branches/trunk/GUI/spot-on.h
+++ branches/trunk/GUI/spot-on.h
@@ -560,9 +560,7 @@ class spoton: public QMainWindow
   QList<QPair<QString, QVariant> > gatherStatistics(void) const;
   QPixmap pixmapForCountry(const QString &country) const;
   QString currentTabName(void) const;
-  QString listenerTransport(void) const;
   QString neighborSummary(QTableWidgetItem *item, int &h, int &v) const;
-  QString participantKeyType(QTableWidget *item) const;
   QString saveCommonUrlCredentials
     (const QPair<QByteArray, QByteArray> &keys,
      const QString &cipherType, const QString &hashType,
@@ -573,7 +571,6 @@ class spoton: public QMainWindow
 		     QWidget *parent);
   bool deleteAllUrls(void);
   bool isKernelActive(void) const;
-  bool listenerSupportsSslTls(void) const;
   bool promptBeforeExit(void);
   bool saveGemini(const QPair<QByteArray, QByteArray> &gemini,
 		  const QString &oid);
