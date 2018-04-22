2fcc689d31ec8a0d8c32f0ca8b818abd2a63f895
Monster <textbrowser@gmail.com>
2018-04-22 10:02:38 -0400

    Away with shalalalalalalalalalabingbatmit!

--- branches/trunk/Shell/goldbug.qt5.sh
+++ branches/trunk/Shell/goldbug.qt5.sh
@@ -3,6 +3,8 @@
 if [ -r /usr/local/goldbug/GoldBug ] && [ -x /usr/local/goldbug/GoldBug ]
 then
     export LD_LIBRARY_PATH=/usr/local/goldbug/Lib
+    # Disable https://en.wikipedia.org/wiki/MIT-SHM.
+    export QT_X11_NO_MITSHM=1
     cd /usr/local/goldbug && exec ./GoldBug -style fusion "$@"
     exit $?
 else
--- branches/trunk/Shell/spot-on.qt5.sh
+++ branches/trunk/Shell/spot-on.qt5.sh
@@ -3,6 +3,8 @@
 if [ -r /usr/local/spot-on/Spot-On ] && [ -x /usr/local/spot-on/Spot-On ]
 then
     export LD_LIBRARY_PATH=/usr/local/spot-on/Lib
+    # Disable https://en.wikipedia.org/wiki/MIT-SHM.
+    export QT_X11_NO_MITSHM=1
     cd /usr/local/spot-on && exec ./Spot-On -style fusion "$@"
     exit $?
 else
