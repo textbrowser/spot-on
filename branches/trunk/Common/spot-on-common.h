9a4945acfbeef1c354a7328f2d924645a8dcce46
Alexis Megas <textbrowser@gmail.com>
2018-08-16 07:00:43 -0400

    New release.

--- branches/trunk/Common/spot-on-common.h
+++ branches/trunk/Common/spot-on-common.h
@@ -31,7 +31,7 @@
 #include <QHash>
 #include <QStringList>
 
-#define SPOTON_VERSION_STR "2018.06.06"
+#define SPOTON_VERSION_STR "2018.08.19"
 
 typedef QHash<QString, QByteArray> QStringByteArrayHash;
 typedef QList<QByteArray> QByteArrayList;
