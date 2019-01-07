ac010d4a16ecd4ff428bd12bf1b2471784527b42
Alexis Megas <textbrowser@gmail.com>
2019-01-07 08:54:22 -0500

    On-top neighbors statistics for Mac and W32.

--- branches/trunk/GUI/spot-on-neighborstatistics.cc
+++ branches/trunk/GUI/spot-on-neighborstatistics.cc
@@ -50,6 +50,9 @@ spoton_neighborstatistics::spoton_neighborstatistics(QWidget *parent):
 	  SLOT(slotTimeout(void)));
   m_timer.setInterval(2500);
   m_timer.start();
+#if defined(Q_OS_MAC) || defined(Q_OS_WIN)
+  setWindowFlags(windowFlags() | Qt::WindowStaysOnTopHint);
+#endif
 #ifdef Q_OS_MAC
   statusBar()->setSizeGripEnabled(false);
 #endif
