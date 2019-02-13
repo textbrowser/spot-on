9cf011987b12983b222b41b6cb6d27786fbb43f3
textbrowser@gmail.com <textbrowser@gmail.com>
2019-02-13 06:52:54 -0500

    Version information.

--- branches/trunk/Common/spot-on-common.h
+++ branches/trunk/Common/spot-on-common.h
@@ -33,7 +33,7 @@
 
 #include <limits>
 
-#define SPOTON_VERSION_STR "2019.01.25"
+#define SPOTON_VERSION_STR "2019.02.20"
 
 typedef QHash<QString, QByteArray> QStringByteArrayHash;
 typedef QList<QByteArray> QByteArrayList;
