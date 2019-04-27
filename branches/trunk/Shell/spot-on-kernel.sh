ddaa6800c4b4c7d8b339a2c0be88d9163290c246
textbrowser@gmail.com <textbrowser@gmail.com>
2019-04-27 19:46:30 -0400

    SO on Qt4.

--- branches/trunk/Shell/spot-on-kernel.sh
+++ branches/trunk/Shell/spot-on-kernel.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if [ -r /usr/local/spot-on/Spot-On-Kernel ] && \
-   [ -x /usr/local/spot-on/Spot-On-Kernel ]
+if [ -r /usr/local/spot-on-qt4/Spot-On-Kernel ] && \
+   [ -x /usr/local/spot-on-qt4/Spot-On-Kernel ]
 then
-    export LD_LIBRARY_PATH=/usr/local/spot-on/Lib
-    cd /usr/local/spot-on && exec ./Spot-On-Kernel "$@"
+    export LD_LIBRARY_PATH=/usr/local/spot-on-qt4/Lib
+    cd /usr/local/spot-on-qt4 && exec ./Spot-On-Kernel "$@"
     exit $?
 else
     exit 1
--- branches/trunk/spot-on-gui.pro
+++ branches/trunk/spot-on-gui.pro
@@ -30,7 +30,7 @@ QMAKE_CXXFLAGS_RELEASE -= -O2
 QMAKE_CXXFLAGS_RELEASE += -fPIE -fstack-protector-all -fwrapv \
                           -mtune=native -pie -O3 \
 			  -Wall -Wcast-align -Wcast-qual \
-			  -Werror -Wextra \
+			  -Wextra \
 			  -Woverloaded-virtual -Wpointer-arith \
                           -Wstack-protector -Wstrict-overflow=5
 QMAKE_DISTCLEAN        += -r temp
