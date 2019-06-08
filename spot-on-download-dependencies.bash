cc3a6b95e983d20eabb7d11e4efb2a1b71f4c4a3
textbrowser@gmail.com <textbrowser@gmail.com>
2019-06-08 14:21:43 -0400

    CURL 7.65.1, PQ 9.6.13.4.

--- spot-on-download-dependencies.bash
+++ spot-on-download-dependencies.bash
@@ -4,12 +4,12 @@
 
 # CURL
 
-curl=curl-7.65.0_1-win32-mingw
+curl=curl-7.65.1-win32-mingw
 
 rm -f $curl.zip
 rm -fr $curl
 wget --progress=bar \
-     https://curl.haxx.se/windows/dl-7.65.0_1/$curl.zip
+     https://curl.haxx.se/windows/dl-7.65.1/curl-7.65.1-win32-mingw.zip
 unzip $curl.zip -d curl-temporary.d
 mv curl-temporary.d/*/bin/curl-ca-bundle.crt libcURL/.
 mv curl-temporary.d/*/bin/libcurl.dll libcURL/Win32.d/bin/.
@@ -42,7 +42,7 @@ postgresql=postgresql.zip
 rm -f $postgresql
 wget --output-document=$postgresql \
      --progress=bar \
-     "https://get.enterprisedb.com/postgresql/postgresql-9.6.13-1-windows-binaries.zip"
+     "https://get.enterprisedb.com/postgresql/postgresql-9.6.13-4-windows-binaries.zip"
 unzip $postgresql
 mv pgsql/bin/libeay32.dll PostgreSQL/Libraries.win32/.
 mv pgsql/bin/libiconv-2.dll PostgreSQL/Libraries.win32/.
