48b52db4d9a0f3bc3b31cb181fce78bf03aabc74
textbrowser@gmail.com <textbrowser@gmail.com>
2019-11-16 10:44:09 -0500

    Default styles.

--- branches/trunk/Shell/spot-on.qt5.sh
+++ branches/trunk/Shell/spot-on.qt5.sh
@@ -5,7 +5,7 @@ then
     export LD_LIBRARY_PATH=/usr/local/spot-on/Lib
     # Disable https://en.wikipedia.org/wiki/MIT-SHM.
     export QT_X11_NO_MITSHM=1
-    cd /usr/local/spot-on && exec ./Spot-On -style Fusion "$@"
+    cd /usr/local/spot-on && exec ./Spot-On "$@"
     exit $?
 else
     exit 1
