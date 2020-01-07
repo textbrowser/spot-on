e422240e94bbfc032d3a08e72eead86813e3eb34
textbrowser@gmail.com <textbrowser@gmail.com>
2020-01-07 12:42:50 -0500

    Moved release.

--- branches/trunk/Common/spot-on-common.h
+++ branches/trunk/Common/spot-on-common.h
@@ -33,7 +33,7 @@
 
 #include <limits>
 
-#define SPOTON_VERSION_STR "2020.01.05"
+#define SPOTON_VERSION_STR "2020.01.20"
 
 typedef QHash<QString, QByteArray> QStringByteArrayHash;
 typedef QList<QByteArray> QByteArrayList;
