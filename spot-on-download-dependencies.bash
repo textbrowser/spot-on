af3e8f2f104166715798d084bf91e238fefc3545
textbrowser@gmail.com <textbrowser@gmail.com>
2020-03-21 20:53:53 -0400

    CURL 7.69.1.

--- spot-on-download-dependencies.bash
+++ spot-on-download-dependencies.bash
@@ -4,12 +4,12 @@
 
 # CURL
 
-curl=curl-7.69.0-win32-mingw
+curl=curl-7.69.1-win32-mingw
 
 rm -f $curl.zip
 rm -fr $curl
 wget --progress=bar \
-     https://curl.haxx.se/windows/dl-7.69.0/curl-7.69.0-win32-mingw.zip
+     https://curl.haxx.se/windows/dl-7.69.1/curl-7.69.1-win32-mingw.zip
 unzip $curl.zip -d curl-temporary.d
 mv curl-temporary.d/*/bin/curl-ca-bundle.crt libcURL/.
 mv curl-temporary.d/*/bin/libcurl.dll libcURL/Win32.d/bin/.
