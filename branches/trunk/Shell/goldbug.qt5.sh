b1996c738cb35773b337a603fd829459f75e1557
textbrowser@gmail.com <textbrowser@gmail.com>
2020-05-26 21:10:01 -0400

    High DPI.

--- branches/trunk/Shell/goldbug.qt5.sh
+++ branches/trunk/Shell/goldbug.qt5.sh
@@ -3,7 +3,10 @@
 if [ -r /usr/local/goldbug/GoldBug ] && [ -x /usr/local/goldbug/GoldBug ]
 then
     export LD_LIBRARY_PATH=/usr/local/goldbug/Lib
+    export QT_AUTO_SCREEN_SCALE_FACTOR=1
+
     # Disable https://en.wikipedia.org/wiki/MIT-SHM.
+
     export QT_X11_NO_MITSHM=1
     cd /usr/local/goldbug && exec ./GoldBug "$@"
     exit $?
--- branches/trunk/Shell/spot-on.qt5.sh
+++ branches/trunk/Shell/spot-on.qt5.sh
@@ -4,7 +4,9 @@ if [ -r /usr/local/spot-on/Spot-On ] && [ -x /usr/local/spot-on/Spot-On ]
 then
     export LD_LIBRARY_PATH=/usr/local/spot-on/Lib
     export QT_AUTO_SCREEN_SCALE_FACTOR=1
+
     # Disable https://en.wikipedia.org/wiki/MIT-SHM.
+
     export QT_X11_NO_MITSHM=1
     cd /usr/local/spot-on && exec ./Spot-On "$@"
     exit $?
