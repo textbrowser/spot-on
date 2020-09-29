a67acf388966cb3f11ec8a4ac719e697bcfa2431
textbrowser@gmail.com <textbrowser@gmail.com>
2020-09-29 11:26:14 -0400

    Doxygen.

--- branches/trunk/Doxygen/html/search/classes_2.js
+++ branches/trunk/Doxygen/html/search/classes_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['private_65',['Private',['../classCocoaInitializer_1_1Private.html',1,'CocoaInitializer']]]
+  ['private_66',['Private',['../classCocoaInitializer_1_1Private.html',1,'CocoaInitializer']]]
 ];
