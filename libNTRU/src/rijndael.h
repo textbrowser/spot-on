541900da5322b9087721ff445a6b0fd0aa677759
textbrowser <textbrowser@gmail.com>
2022-08-02 11:26:12 -0400

    Exact function parameters.

--- libNTRU/src/rijndael.h
+++ libNTRU/src/rijndael.h
@@ -52,7 +52,7 @@ void	 rijndael_encrypt(const rijndael_ctx *, const u_char *src, u_char *dst);
 */
 int	rijndaelKeySetupEnc(unsigned int [], const unsigned char [], int);
 int	rijndaelKeySetupDec(unsigned int [], const unsigned char [], int);
-void	rijndaelEncrypt(const unsigned int [], int, const unsigned char [],
-	    unsigned char []);
+void	rijndaelEncrypt(const u32 [], int, const u8 [16],
+	    u8 [16]);
 
 #endif /* __RIJNDAEL_H */
