1347e65cdbe94184c57f83a92034cd12ae8bee5a
textbrowser <textbrowser@gmail.com>
2023-02-10 19:13:01 -0500

    New reminder.

--- spot-on-download-dependencies.bash
+++ spot-on-download-dependencies.bash
@@ -86,7 +86,7 @@ fi
 
 # OpenSSL 1.1.1
 
-openssl=mingw-w64-i686-openssl-1.1.1.s-1-any.pkg.tar.zst
+openssl=mingw-w64-i686-openssl-1.1.1.t-1-any.pkg.tar.zst
 
 rm -f $openssl
 wget --output-document=$openssl \
