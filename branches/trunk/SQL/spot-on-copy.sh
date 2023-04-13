043d9e34fcf4cb36ed3d1289637bc727d914293d
textbrowser <textbrowser@gmail.com>
2023-04-13 12:09:24 -0400

    Local files.

--- branches/trunk/SQL/spot-on-copy.sh
+++ branches/trunk/SQL/spot-on-copy.sh
@@ -2,8 +2,6 @@
 
 date=$(date "+%Y%m%d")
 
-pg_dump -U postgres \
-	--clean --file=/disk0/spot-on.d/spot-on-user-db.$date.sql spot_on_user_db
-pg_dumpall -U postgres \
-	   --clean --globals-only --file=/disk0/spot-on.d/globals.$date.sql
-gzip --force --keep /disk0/spot-on.d/spot-on-user-db.$date.sql
+pg_dump -U postgres --clean --file=spot-on-user-db.$date.sql spot_on_user_db
+pg_dumpall -U postgres --clean --globals-only --file=globals.$date.sql
+gzip --force --keep spot-on-user-db.$date.sql
