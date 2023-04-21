7a2868bdb485dead98c3b9932a33f401d4ed1f8a
textbrowser <textbrowser@gmail.com>
2023-04-21 14:08:08 -0400

    Test script.

--- branches/trunk/Scripts/nc-flood-test.bash
+++ branches/trunk/Scripts/nc-flood-test.bash
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # A test script.
 
-cat Spot-On Spot-On Spot-On Spot-On Spot-On Spot-On \
-Spot-On Spot-On Spot-On Spot-On Spot-On Spot-On Spot-On \
-Spot-On Spot-On Spot-On Spot-On Spot-On Spot-On Spot-On - | nc 127.0.0.1 4710
+cat Spot-On Spot-On Spot-On Spot-On Spot-On \
+    Spot-On Spot-On Spot-On Spot-On Spot-On \
+    Spot-On Spot-On Spot-On Spot-On Spot-On \
+    Spot-On Spot-On Spot-On Spot-On Spot-On \
+    Spot-On Spot-On Spot-On Spot-On Spot-On - | nc 127.0.0.1 4710
