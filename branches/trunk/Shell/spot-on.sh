a263eed56fbf5b8212daac9ea21bf45d9579cba5
textbrowser <textbrowser@gmail.com>
2023-07-09 19:51:37 -0400

    Prefer /opt.

--- branches/trunk/Shell/spot-on.sh
+++ branches/trunk/Shell/spot-on.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 # Alexis Megas.
 
-if [ -r /opt/Spot-On ] && [ -x /opt/spot-on/Spot-On ]
+if [ -r /opt/spot-on/Spot-On ] && [ -x /opt/spot-on/Spot-On ]
 then
     export LD_LIBRARY_PATH=/opt/spot-on/Lib
     export QT_AUTO_SCREEN_SCALE_FACTOR=1
