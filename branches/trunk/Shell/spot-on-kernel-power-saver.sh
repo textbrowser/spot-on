094dd9bd7377624925a52eadb9b9bfb1bf485b85
textbrowser <textbrowser@gmail.com>
2023-10-09 09:42:37 -0400

    New program.

---
+++ branches/trunk/Shell/spot-on-kernel-power-saver.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# Alexis Megas.
+
+if [ -r /opt/spot-on/Spot-On-Kernel ] && [ -x /opt/spot-on/Spot-On-Kernel ]
+then
+    export LD_LIBRARY_PATH=/opt/spot-on/Lib
+    cd /opt/spot-on && exec ./Spot-On-Kernel \
+			    --disable-mail \
+			    --disable-starbeam \
+			    --disable-ui-server \
+			    "$@"
+    exit $?
+else
+    echo "Could not locate /opt/spot-on/Spot-On-Kernel."
+    exit 1
+fi
