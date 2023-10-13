1fa4839eb0733729e71e68e40685a595a0a37007
textbrowser <textbrowser@gmail.com>
2023-10-13 18:11:39 -0400

    Updated.

--- branches/trunk/version.sh
+++ branches/trunk/version.sh
@@ -7,16 +7,17 @@ if [ -z "$VERSION" ]; then
     exit 1
 fi
 
-for file in Distributions/build*; do
-    sed -i "s/Spot-On-.*_/Spot-On-$VERSION\_/" $file
-done
-
 for file in */control; do
     sed -i "s/Version: .*/Version: $VERSION/" $file
 done
 
+for file in Distributions/build*; do
+    sed -i "s/Spot-On-.*_/Spot-On-$VERSION\_/" $file
+done
+
 FILE="Common/spot-on-version.h"
 
-sed -i 's/\(SPOTON_VERSION_STR "\)[0-9]\+\(\.[0-9]\+\)*"/\1'"$VERSION"'"/' $FILE
+sed -i 's/\(SPOTON_VERSION_STR "\)[0-9]\+\(\.[0-9]\+\)*"/\1'"$VERSION"'"/' \
+    $FILE
 
 echo "Please modify ReleaseNotes.html."
