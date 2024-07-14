691ecb4389e6caa7711654d02b9a269a8d0bbc27
textbrowser <textbrowser@gmail.com>
2024-07-14 18:50:17 -0400

    MacOS.

--- branches/trunk/Shell/spot-on-kernel.sh
+++ branches/trunk/Shell/spot-on-kernel.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env sh
 # Alexis Megas.
 
+os=$(uname -o 2>/dev/null)
+
+if [ "$os" == "Darwin" ]
+then
+    if [ -x ./Spot-On-Kernel.app/Contents/MacOS/Spot-On-Kernel ]
+    then
+	export DYLD_LIBRARY_PATH=../../libNTRU
+	exec ./Spot-On-Kernel.app/Contents/MacOS/Spot-On-Kernel "$@"
+	exit $?
+    else
+	echo "Missing ./Spot-On-Kernel.app/Contents/MacOS/Spot-On-Kernel."
+	exit 1
+    fi
+fi
+
 if [ -r /opt/spot-on/Spot-On-Kernel ] && [ -x /opt/spot-on/Spot-On-Kernel ]
 then
     export LD_LIBRARY_PATH=/opt/spot-on/Lib
