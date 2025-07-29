1f1b7088ba3a8b48984a2ae6a7fac1712b93fd00
textbrowser <textbrowser@gmail.com>
2025-07-29 15:48:49 -0400

    MacOS.

--- Source/Shell/spot-on-kernel-power-saver.sh
+++ Source/Shell/spot-on-kernel-power-saver.sh
@@ -2,6 +2,28 @@
 
 # Alexis Megas.
 
+os=$(uname -o 2>/dev/null)
+
+if [ "$os" = "Darwin" ]
+then
+    if [ -x ./Spot-On-Kernel.app/Contents/MacOS/Spot-On-Kernel ]
+    then
+	export DYLD_LIBRARY_PATH=../libNTRU
+	./Spot-On-Kernel.app/Contents/MacOS/Spot-On-Kernel \
+	    --disable-mail \
+	    --disable-poptastic \
+	    --disable-starbeam \
+	    --disable-ui-server \
+	    --passphrase \
+	    "@"
+	exit $?
+    else
+	echo "Could not locate ./Spot-On-Kernel.app/" \
+	     "Contents/MacOS/Spot-On-Kernel."
+	exit 1
+    fi
+fi
+
 if [ -r /opt/spot-on/Spot-On-Kernel ] && [ -x /opt/spot-on/Spot-On-Kernel ]
 then
     export LD_LIBRARY_PATH=/opt/spot-on/Lib
