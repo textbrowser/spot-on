9858c827e46edac68bb72a43240c1d5ae7a8976b
textbrowser <textbrowser@gmail.com>
2026-01-18 19:05:40 -0500

    Quotes.

--- spot-on-download-dependencies.bash
+++ spot-on-download-dependencies.bash
@@ -11,22 +11,21 @@ then
     exit 1
 fi
 
+msys2=https://repo.msys2.org/mingw/mingw64
 rc=0
 
 # Assuan
 
 assuan=mingw-w64-x86_64-libassuan-2.5.7-1-any.pkg.tar.zst
 
-rm -f $assuan
-wget --output-document=$assuan \
-     --progress=bar \
-     https://repo.msys2.org/mingw/mingw64/$assuan
+rm -f "$assuan"
+wget --output-document="$assuan" --progress=bar "$msys2/$assuan"
 
 rc=$?
 
 if [ $rc -eq 0 ] && [ -r "$assuan" ]
 then
-    tar -I zstd -vxf $assuan
+    tar -I zstd -vxf "$assuan"
     mkdir -p libAssuan/Libraries.win64
     mv mingw64/bin/*.dll libAssuan/Libraries.win64/.
     chmod +w,-x libAssuan/Libraries.win64/*.dll*
@@ -38,22 +37,20 @@ then
     rc=1
 fi
 
-rm -f $assuan
+rm -f "$assuan"
 
 # GCrypt
 
 gcrypt=mingw-w64-x86_64-libgcrypt-1.11.2-1-any.pkg.tar.zst
 
-rm -f $gcrypt
-wget --output-document=$gcrypt \
-     --progress=bar \
-     https://repo.msys2.org/mingw/mingw64/$gcrypt
+rm -f "$gcrypt"
+wget --output-document="$gcrypt" --progress=bar "$msys2/$gcrypt"
 
 rc=$?
 
 if [ $rc -eq 0 ] && [ -r "$gcrypt" ]
 then
-    tar -I zstd -vxf $gcrypt
+    tar -I zstd -vxf "$gcrypt"
     mkdir -p libGCrypt/Include.win64
     mkdir -p libGCrypt/Libraries.win64
     mv mingw64/bin/*.dll libGCrypt/Libraries.win64/.
@@ -67,22 +64,20 @@ then
     rc=1
 fi
 
-rm -f $gcrypt
+rm -f "$gcrypt"
 
 # GPG-Error
 
 gpgerror=mingw-w64-x86_64-libgpg-error-1.55-1-any.pkg.tar.zst
 
-rm -f $gpgerror
-wget --output-document=$gpgerror \
-     --progress=bar \
-     https://repo.msys2.org/mingw/mingw64/$gpgerror
+rm -f "$gpgerror"
+wget --output-document="$gpgerror" --progress=bar "$msys2/$gpgerror"
 
 rc=$?
 
 if [ $rc -eq 0 ] && [ -r "$gpgerror" ]
 then
-    tar -I zstd -vxf $gpgerror
+    tar -I zstd -vxf "$gpgerror"
     mkdir -p libGCrypt/Include.win64
     mkdir -p libGCrypt/Libraries.win64
     mv mingw64/bin/*.dll libGCrypt/Libraries.win64/.
@@ -102,16 +97,14 @@ rm -f $gpgerror
 
 gpgme=mingw-w64-x86_64-gpgme-1.23.2-11-any.pkg.tar.zst
 
-rm -f $gpgme
-wget --output-document=$gpgme \
-     --progress=bar \
-     https://repo.msys2.org/mingw/mingw64/$gpgme
+rm -f "$gpgme"
+wget --output-document=$gpgme --progress=bar "$msys2/$gpgme"
 
 rc=$?
 
 if [ $rc -eq 0 ] && [ -r "$gpgme" ]
 then
-    tar -I zstd -vxf $gpgme
+    tar -I zstd -vxf "$gpgme"
     mkdir -p libGPGME/Executables.win64
     mkdir -p libGPGME/Include.win64
     mkdir -p libGPGME/Libraries.win64
@@ -127,23 +120,23 @@ then
     rc=1
 fi
 
-rm -f $gpgme
+rm -f "$gpgme"
 
 # OpenSSL
 
 openssl=openssl-3-6-zip
 
-rm -f $openssl
-wget --output-document=$openssl \
+rm -f "$openssl"
+wget --output-document="$openssl" \
      --progress=bar \
-     https://www.firedaemon.com/download-firedaemon-$openssl
+     "https://www.firedaemon.com/download-firedaemon-$openssl"
 
 rc=$?
 
 if [ $rc -eq 0 ] && [ -r "$openssl" ]
 then
     rm -fr openssl.d
-    unzip $openssl -d openssl.d
+    unzip "$openssl" -d openssl.d
     mkdir -p libOpenSSL/Executables.win64
     mkdir -p libOpenSSL/Include.win64
     mkdir -p libOpenSSL/Libraries.win64
@@ -161,5 +154,5 @@ then
     rc=1
 fi
 
-rm -f $openssl
+rm -f "$openssl"
 exit $rc
