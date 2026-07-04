6372af5c3387a9568a8e990045518f46a55622a0
textbrowser@gmail.com <textbrowser@gmail.com>
2026-07-04 19:48:37 -0400

    Intelligence?

--- spot-on-download-dependencies.bash
+++ spot-on-download-dependencies.bash
@@ -11,21 +11,47 @@ then
     exit 1
 fi
 
+mingw64="mingw64.html"
 msys2=https://repo.msys2.org/mingw/mingw64
 rc=0
 
+echo "Downloading $msys2."
+rm -f "$mingw64"
+wget --output-document="$mingw64" \
+     --progress=bar \
+     --quiet \
+     https://repo.msys2.org/mingw/mingw64
+
+rc=$?
+
+if [ ! $rc -eq 0 ]
+then
+    echo "The command wget failed."
+    exit $rc
+fi
+
 # Assuan
 
-assuan=mingw-w64-x86_64-libassuan-2.5.7-1-any.pkg.tar.zst
+assuan="$(cat "$mingw64" | grep mingw-w64-x86_64-libassuan | \
+	  grep -Po '"\K[^"]+' | \
+	  sort --version-sort | grep '^m' | tail -2 | head -1)"
 
+if [ -z "$assuan" ]
+then
+    echo "Cannot find assuan name."
+    rm -fr "$mingw64"
+    exit 1
+fi
+
+echo "Downloading $assuan."
 rm -f "$assuan"
-wget --output-document="$assuan" --progress=bar "$msys2/$assuan"
+wget --output-document="$assuan" --progress=bar --quiet "$msys2/$assuan"
 
 rc=$?
 
 if [ $rc -eq 0 ] && [ -r "$assuan" ]
 then
-    tar -I zstd -vxf "$assuan"
+    tar -I zstd -vxf "$assuan" 1>/dev/null
     mkdir -p libAssuan/Libraries.win64
     mv mingw64/bin/*.dll libAssuan/Libraries.win64/.
     chmod +w,-x libAssuan/Libraries.win64/*.dll*
@@ -41,16 +67,26 @@ rm -f "$assuan"
 
 # GCrypt
 
-gcrypt=mingw-w64-x86_64-libgcrypt-1.12.2-2-any.pkg.tar.zst
+gcrypt="$(cat "$mingw64" | grep mingw-w64-x86_64-libgcrypt | \
+	  grep -Po '"\K[^"]+' | \
+	  sort --version-sort | grep '^m' | tail -2 | head -1)"
 
+if [ -z "$gcrypt" ]
+then
+    echo "Cannot find gcrypt name."
+    rm -fr "$mingw64"
+    exit 1
+fi
+
+echo "Downloading $gcrypt."
 rm -f "$gcrypt"
-wget --output-document="$gcrypt" --progress=bar "$msys2/$gcrypt"
+wget --output-document="$gcrypt" --progress=bar --quiet "$msys2/$gcrypt"
 
 rc=$?
 
 if [ $rc -eq 0 ] && [ -r "$gcrypt" ]
 then
-    tar -I zstd -vxf "$gcrypt"
+    tar -I zstd -vxf "$gcrypt" 1>/dev/null
     mkdir -p libGCrypt/Include.win64
     mkdir -p libGCrypt/Libraries.win64
     mv mingw64/bin/*.dll libGCrypt/Libraries.win64/.
@@ -68,16 +104,26 @@ rm -f "$gcrypt"
 
 # GPG-Error
 
-gpgerror=mingw-w64-x86_64-libgpg-error-1.60-1-any.pkg.tar.zst
+gpgerror="$(cat "$mingw64" | grep mingw-w64-x86_64-libgpg-error | \
+	    grep -Po '"\K[^"]+' | \
+	    sort --version-sort | grep '^m' | tail -2 | head -1)"
 
+if [ -z "$gcrypt" ]
+then
+    echo "Cannot find gpgerror name."
+    rm -fr "$mingw64"
+    exit 1
+fi
+
+echo "Downloading $gpgerror."
 rm -f "$gpgerror"
-wget --output-document="$gpgerror" --progress=bar "$msys2/$gpgerror"
+wget --output-document="$gpgerror" --progress=bar --quiet "$msys2/$gpgerror"
 
 rc=$?
 
 if [ $rc -eq 0 ] && [ -r "$gpgerror" ]
 then
-    tar -I zstd -vxf "$gpgerror"
+    tar -I zstd -vxf "$gpgerror" 1>/dev/null
     mkdir -p libGCrypt/Include.win64
     mkdir -p libGCrypt/Libraries.win64
     mv mingw64/bin/*.dll libGCrypt/Libraries.win64/.
@@ -95,16 +141,26 @@ rm -f $gpgerror
 
 # GPG-ME
 
-gpgme=mingw-w64-x86_64-gpgme-1.23.2-17-any.pkg.tar.zst
+gpgme="$(cat "$mingw64" | grep mingw-w64-x86_64-gpgme | \
+	 grep -Po '"\K[^"]+' | \
+         sort --version-sort | grep '^m' | tail -2 | head -1)"
 
+if [ -z "$gpgme" ]
+then
+    echo "Cannot find gpgme name."
+    rm -fr "$mingw64"
+    exit 1
+fi
+
+echo "Downloading $gpgme."
 rm -f "$gpgme"
-wget --output-document="$gpgme" --progress=bar "$msys2/$gpgme"
+wget --output-document="$gpgme" --progress=bar --quiet "$msys2/$gpgme"
 
 rc=$?
 
 if [ $rc -eq 0 ] && [ -r "$gpgme" ]
 then
-    tar -I zstd -vxf "$gpgme"
+    tar -I zstd -vxf "$gpgme" 1>/dev/null
     mkdir -p libGPGME/Executables.win64
     mkdir -p libGPGME/Include.win64
     mkdir -p libGPGME/Libraries.win64
@@ -126,9 +182,11 @@ rm -f "$gpgme"
 
 openssl=openssl-3.6.2.zip
 
+echo "Downloading $openssl."
 rm -f "$openssl"
 wget --output-document="$openssl" \
      --progress=bar \
+     --quiet \
      "https://download.firedaemon.com/FireDaemon-OpenSSL/$openssl"
 
 rc=$?
@@ -136,7 +194,7 @@ rc=$?
 if [ $rc -eq 0 ] && [ -r "$openssl" ]
 then
     rm -fr openssl.d
-    unzip "$openssl" -d openssl.d
+    unzip "$openssl" -d openssl.d 1>/dev/null
     mkdir -p libOpenSSL/Executables.win64
     mkdir -p libOpenSSL/Include.win64
     mkdir -p libOpenSSL/Libraries.win64
@@ -154,5 +212,6 @@ then
     rc=1
 fi
 
+rm -f "$mingw64"
 rm -f "$openssl"
 exit $rc
